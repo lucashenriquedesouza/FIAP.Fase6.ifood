561671cc053d3636eb0b9055479bbd52be5dc4fd
Lucas Souza <lucashenriquedesouzall@gmail.com>
2021-11-28 17:03:21 -0300

    Adicionado docker compose e git igonore

---
+++ FIAP.Fase6.ifood/FIAP.Fase6.ifood.Restaurantes.Command/Controllers/NotificacaoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIAP.Fase6.ifood.Restaurantes.Command.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificacaoController : ControllerBase
+    {
+    }
+}
