f4622e8490f1601acdb367b1c3a1cfd781036bf4
Lucas Souza <lucashenriquedesouzall@gmail.com>
2021-11-28 23:13:31 -0300

    Correção em viewmodel

--- FIAP.Fase6.ifood/FIAP.Fase6.ifood.Restaurantes.Application/ViewModels/ProdutoViewModel.cs
+++ FIAP.Fase6.ifood/FIAP.Fase6.ifood.Restaurantes.Application/ViewModels/ProdutoViewModel.cs
@@ -32,6 +32,6 @@ namespace FIAP.Fase6.ifood.Restaurantes.Application.ViewModels
         public decimal Valor { get; set; }
 
         [Required(ErrorMessage = "Ativo Requerido")]
-        public bool Atvo { get; set; }
+        public bool Ativo { get; set; } = true;
     }
 }
